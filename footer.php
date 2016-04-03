b3bfa1e7b6fea082592035182d9d3a6a6c1f8671
Sarah Hamdi <s.hamdi@gmail.com>
2016-04-03 13:02:07 -0400

    Getting content on the homepage

--- footer.php
+++ footer.php
@@ -11,16 +11,6 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'retro-film-festival' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'retro-film-festival' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'retro-film-festival' ), 'retro-film-festival', '<a href="http://underscores.me/" rel="designer">sarah-hamdi</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
